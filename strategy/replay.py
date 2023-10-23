9f983609c0ffc1cd61e4e3cf194620506ac9c9e6
Tremo8 <matteo.tremonti@gmail.com>
2023-10-23 14:31:14 +0200

    Expirience Replay strategy

--- strategy/replay.py
+++ strategy/replay.py
@@ -2,6 +2,8 @@ import torch
 from torch.utils.data import DataLoader, ConcatDataset
 
 from avalanche.benchmarks.utils.data_loader import ReplayDataLoader
+from avalanche.training.storage_policy import ClassBalancedBuffer
+from avalanche.benchmarks.utils import classification_subset
 
 from strategy.base_strategy import BaseStrategy
 
@@ -9,7 +11,7 @@ import utility.utils as utils
 
 class Replay(BaseStrategy):
     """ Experience replay strategy. """
-    def __init__(self, model, optimizer, criterion, train_mb_size, train_epochs, eval_mb_size, storage_policy, split_ratio = 0, patience = 5, device="cpu", path = None):
+    def __init__(self, model, optimizer, criterion, train_mb_size, train_epochs, eval_mb_size, rm_size, mem_mb_size = None, storage_policy = None, split_ratio = 0, patience = 5, device="cpu", file_name = None, path = None):
         """Init.
 
         Args:
@@ -19,10 +21,12 @@ class Replay(BaseStrategy):
             train_mb_size: training mini-batch size.
             train_epochs: number of training epochs.
             eval_mb_size: evaluation mini-batch size.
+            rm_size: size of the memory.
             storage_policy: storage policy.
             split_ratio: ratio to split the dataset into training and validation.  If 0, no early stopping is performed.
             patience: patience for early stopping.
             device: PyTorch device where the model will be allocated.
+            file_name: name of the file to save the results.
             path: path to save the model.
         """
         super().__init__(
@@ -35,11 +39,17 @@ class Replay(BaseStrategy):
             split_ratio = split_ratio,
             patience=patience,
             device=device,
+            file_name = file_name,
             path = path
         )
 
-        self.storage_policy = storage_policy
+        if storage_policy is not None:
+            self.storage_policy = storage_policy
+        else:
+            self.storage_policy = ClassBalancedBuffer(rm_size)
         """ Storage policy. """
+
+        self.mem_mb_size = mem_mb_size
 
     def before_training_exp(self, dataset, shuffle: bool = True):
         """ 
@@ -62,13 +72,16 @@ class Replay(BaseStrategy):
 
         # replay dataloader samples mini-batches from the memory and current
         # data separately and combines them together.
+        if self.mem_mb_size is None:
+            self.mem_mb_size = self.train_mb_size
+
         print("Override the dataloader.")
         dataloader = ReplayDataLoader(
             dataset,
             self.storage_policy.buffer,
             oversample_small_tasks=True,
-            batch_size=self.train_mb_size,
-            batch_size_mem  = self.train_mb_size,
+            batch_size = self.train_mb_size,
+            batch_size_mem  = self.mem_mb_size,
             shuffle=shuffle)
         
         return dataloader
@@ -89,22 +102,27 @@ class Replay(BaseStrategy):
             print("Training of the experience with class: ", exp.classes_in_this_experience)
             if self.split_ratio != 0:
                 train_dataset, val_dataset = utils.split_dataset(exp.dataset, split_ratio=self.split_ratio)
+                train_dataset, val_dataset = classification_subset(dataset=exp.dataset, indices=train_dataset.indices), classification_subset(dataset=exp.dataset, indices=val_dataset.indices)
                 print("Train dataset size: ", len(train_dataset))
                 print("Validation dataset size: ", len(val_dataset))
                 val_loader = DataLoader(val_dataset, batch_size=self.eval_mb_size, shuffle=True)
             else:
                 train_dataset = exp.dataset
                 val_loader = None
-            train_loader = self.before_training_exp(train_dataset.dataset, shuffle=True)
+
+            train_loader = self.before_training_exp(train_dataset, shuffle=True)
 
             super().train(train_loader, val_loader)
 
-            self.storage_policy.update_from_dataset(train_dataset.dataset)
+            self.storage_policy.update_from_dataset(train_dataset)
+
             if test_data is not None:
-                print("")
-                print("Test after the training of the experience with class: ", exp.classes_in_this_experience)
                 exps_acc, _ = self.test(test_data)
                 self.update_tasks_acc(exps_acc)
+
+        if self.path is not None:
+            torch.save(self.model.state_dict(), self.path)
+
         if plotting:
             plotter = utils.TaskAccuracyPlotter()
             _ = plotter.plot_task_accuracy(self.tasks_acc, plot_task_acc=True, plot_avg_acc=True, plot_encountered_avg=True)
